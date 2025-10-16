7d536c7ab8c998b5f7e2a01d1c626676c27b72d9
Arthur Souza Sepp <arthursouzasepp@gmail.com>
2025-10-15 22:03:49 -0300

    fix: Ajuste na criptografia da senha do usuário

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import (
     Unidade, Pessoa, Departamento, Curso, Disciplina, Semestre, Turma,
     Matricula, Frequencia, Avaliacao, Nota, ResultadoFinal
 )
 
+@admin.register(Pessoa)
+class PessoaAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Informações adicionais", {
+            "fields": (
+                "numero_matricula",
+                "numero_documento",
+                "turno",
+                "papel",
+                "situacao",
+                "unidade",
+                "foto_perfil",
+            ),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Informações adicionais", {
+            "fields": (
+                "numero_matricula",
+                "numero_documento",
+                "turno",
+                "papel",
+                "situacao",
+                "unidade",
+                "foto_perfil",
+            ),
+        }),
+    )
+
 admin.site.register(Unidade)
-admin.site.register(Pessoa)
+# admin.site.register(Pessoa)
 admin.site.register(Departamento)
 admin.site.register(Curso)
 admin.site.register(Disciplina)
